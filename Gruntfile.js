f3ade24b562d48508254d725ee51d7165707aaa9
William Patton <will@pattonwebz.com>
2017-03-08 23:23:09 +0000

    Added watch task, fixed typo .css -> .js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,8 @@ module.exports = function(grunt) {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
 			build: {
-				src: 'src/js/scripts.css',
-				dest: 'js/scripts.min.css'
+				src: 'src/js/scripts.js',
+				dest: 'js/scripts.min.js'
 			}
 		},
 		cssmin: {
@@ -17,19 +17,31 @@ module.exports = function(grunt) {
 				src: 'src/css/style.css',
 				dest: 'css/style.min.css'
 			}
-		}
+		},
+		watch: {
+			uglify: {
+				files: 'src/js/scripts.js',
+				tasks: ['uglify']
+			},
+			cssmin: {
+				files: 'src/css/style.css',
+				tasks: ['cssmin']
+			}
+        }
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	// Load the plugin that provides the "cssmin" task.
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	// Load the plugin that provides the "watch" task.
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Uglify task
-	grunt.registerTask('test', ['uglify']);
+	grunt.registerTask('scripts', ['uglify']);
 	// CSSMin task
-	grunt.registerTask('test', ['cssmin']);
+	grunt.registerTask('styles', ['cssmin']);
 	// Default task(s).
-	grunt.registerTask('default', ['uglify', 'cssmin']);
+	grunt.registerTask('default', ['uglify', 'cssmin', 'watch']);
 
 };
